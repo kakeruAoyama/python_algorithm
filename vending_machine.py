7935bb860e31211822b05a2bbc64855d9d162e39
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-01-10 19:00:34 +0900

    divmodメソッドを使って実装

--- vending_machine.py
+++ vending_machine.py
@@ -19,6 +19,5 @@ if change < 0:
 coins = [5000, 1000, 500, 100, 50, 10, 5, 1]
 
 for i in coins:
-    r = change // i
-    change %= i
+    r, change = divmod(change, i)
     print(str(i) + ': ' + str(r))
