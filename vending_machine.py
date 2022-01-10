afaad9e8907db97919091b1b935e0d66c9738c56
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-01-10 18:58:28 +0900

    不正な入力を弾く

--- vending_machine.py
+++ vending_machine.py
@@ -1,6 +1,20 @@
+import sys
+
 insert_price = input('insert: ')
+if not insert_price.isdecimal():
+    print('整数を入力してください')
+    sys.exit()
+
 product_price = input('product: ')
+if not product_price.isdecimal():
+    print('整数を入力してください')
+    sys.exit()
+
 change = int(insert_price) - int(product_price)
+
+if change < 0:
+    print('金額が不足しています')
+    sys.exit()
 
 coins = [5000, 1000, 500, 100, 50, 10, 5, 1]
 
