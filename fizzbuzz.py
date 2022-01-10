86d9ef2a40f632dc30352ead37f5bbc0c595906c
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-01-10 18:28:16 +0900

    15で割り切れる場合、FizzBuzzを出力

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 for i in range(1, 51):
-    if i % 3 == 0:
+    if i % 15 == 0:
+        print('FizzBuzz', end=' ')
+    elif i % 3 == 0:
         print('Fizz', end=' ')
     elif i % 5 == 0:
         print('Buzz', end=' ')
