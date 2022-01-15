50ea3873ca4ab2e9d14b5b20aa73eb58dc1f7d4c
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-01-15 18:29:50 +0900

    関数で変換

--- convert.py
+++ convert.py
@@ -7,10 +7,15 @@
 # print(result)
 
 
-n = '10010'
-result = 0
+# n = '10010'
+# result = 0
 
-for i in range(len(n)):
-    result += int(n[i]) * (2 ** (len(n) - i -1))
-print(result)
+# for i in range(len(n)):
+#     result += int(n[i]) * (2 ** (len(n) - i -1))
+# print(result)
 
+a = 18
+print(bin(a))
+
+b = '10010'
+print(int(b, 2))
