eb8406db96d1944a9ddc48c38eab23f5f4d9fa6b
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-01-15 18:29:01 +0900

    2進数から10進数へ変換

--- convert.py
+++ convert.py
@@ -1,8 +1,16 @@
-n = 18
-result = ''
+# n = 18
+# result = ''
 
-while n > 0:
-    result = str(n % 2) + result
-    n //= 2
+# while n > 0:
+#     result = str(n % 2) + result
+#     n //= 2
+# print(result)
 
+
+n = '10010'
+result = 0
+
+for i in range(len(n)):
+    result += int(n[i]) * (2 ** (len(n) - i -1))
 print(result)
+
